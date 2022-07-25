d07e0770d3d7c17a28df60c409e685369d4a5b19
SirNoob97 <martinald467@gmail.com>
2022-07-25 20:24:17 +0200

    create spring project

---
+++ src/test/java/com/sirnoob97/storageservice/StorageserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirnoob97.storageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
