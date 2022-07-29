86e1d033d1a15fe756ff91b29fff876ed905fea6
SirNoob97 <martinald467@gmail.com>
2022-07-29 21:28:34 +0200

    getters

--- src/main/java/com/sirnoob97/storageservice/file/dto/FileDto.java
+++ src/main/java/com/sirnoob97/storageservice/file/dto/FileDto.java
@@ -1,15 +1,16 @@
 package com.sirnoob97.storageservice.file.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 class FileDto {
 
   public String fileName;
   public Long fileSize;
   public String mimeType;
   public Byte[] fileData;
-
 }
--- src/main/java/com/sirnoob97/storageservice/file/dto/FileInfoDto.java
+++ src/main/java/com/sirnoob97/storageservice/file/dto/FileInfoDto.java
@@ -1,14 +1,19 @@
 package com.sirnoob97.storageservice.file.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 class FileInfoDto {
 
   public String fileName;
   public Long fileSize;
   public String mimeType;
+
+  @Setter
   public String downloadUrl;
 }
