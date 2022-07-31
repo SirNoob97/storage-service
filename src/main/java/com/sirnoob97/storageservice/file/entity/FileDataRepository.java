299479f033f1be11b8f9254fc79016180e39f8bd
SirNoob97 <martinald467@gmail.com>
2022-07-31 21:50:30 +0200

    this repository is mandatory, a `File` entity needs a `FileData` entity already persisted due the ono to one relationship

---
+++ src/main/java/com/sirnoob97/storageservice/file/entity/FileDataRepository.java
@@ -0,0 +1,6 @@
+package com.sirnoob97.storageservice.file.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileDataRepository extends JpaRepository<FileData, Long> {
+}
