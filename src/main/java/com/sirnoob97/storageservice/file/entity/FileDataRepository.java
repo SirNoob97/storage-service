93b703d55dc9cb21dfd5e643c621f4bac5bd4ce1
SirNoob97 <martinald467@gmail.com>
2022-08-02 20:45:08 +0200

    repository annotation

--- src/main/java/com/sirnoob97/storageservice/file/entity/FileDataRepository.java
+++ src/main/java/com/sirnoob97/storageservice/file/entity/FileDataRepository.java
@@ -1,6 +1,8 @@
 package com.sirnoob97.storageservice.file.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FileDataRepository extends JpaRepository<FileData, Long> {
 }
