4602b3c0f0b18644a87d1de5baf3ce15774bf928
SirNoob97 <martinald467@gmail.com>
2022-08-02 21:16:38 +0200

    more save method use cases

--- src/test/java/com/sirnoob97/storageservice/file/repository/FileDataRepositoryTest.java
+++ src/test/java/com/sirnoob97/storageservice/file/repository/FileDataRepositoryTest.java
@@ -3,13 +3,17 @@ package com.sirnoob97.storageservice.file.repository;
 import static com.sirnoob97.storageservice.util.EntityGenerator.randomFileData;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import javax.transaction.Transactional;
+import com.sirnoob97.storageservice.file.entity.FileData;
 import com.sirnoob97.storageservice.file.entity.FileDataRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 
 @DataJpaTest
 @Transactional
@@ -31,5 +35,16 @@ class FileDataRepositoryTest {
 
     assertEquals(fileData.getId(), fileDataDb.getId());
     assertEquals(fileData.getFileData(), fileDataDb.getFileData());
+  }
+
+  @Test
+  void test_Save_ThrowInvalidDataAccessApiUsageException_WhenFileDataParameterIsNull() {
+    assertThrows(InvalidDataAccessApiUsageException.class, () -> fileDataRepository.save(null));
+  }
+
+  @Test
+  void test_Save_ThrowDataIntegrityViolationException_WhenFileDataByteArrayIsNull() {
+    var fileData = FileData.builder().fileData(null).build();
+    assertThrows(DataIntegrityViolationException.class, () -> fileDataRepository.save(fileData));
   }
 }
