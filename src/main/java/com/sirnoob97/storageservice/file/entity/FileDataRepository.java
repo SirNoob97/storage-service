6ce27cf5acfa63fbfb9dfd06ab1d05bb2621a253
SirNoob97 <martinald467@gmail.com>
2022-08-07 17:18:00 +0200

    native queries for delete methods

--- src/main/java/com/sirnoob97/storageservice/file/entity/FileDataRepository.java
+++ src/main/java/com/sirnoob97/storageservice/file/entity/FileDataRepository.java
@@ -1,8 +1,14 @@
 package com.sirnoob97.storageservice.file.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FileDataRepository extends JpaRepository<FileData, Long> {
+
+  @Modifying
+  @Query(value = "DELETE FROM file_data WHERE file_data.id = ?", nativeQuery = true)
+  int deleteFileDataById(Long id);
 }
