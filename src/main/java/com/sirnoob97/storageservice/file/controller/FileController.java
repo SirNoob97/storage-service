4cc29d89720baba16398f9b431bb9affb86c73ff
SirNoob97 <martinald467@gmail.com>
2022-08-13 18:33:10 +0200

    PUT is related to updates operations

--- src/main/java/com/sirnoob97/storageservice/file/controller/FileController.java
+++ src/main/java/com/sirnoob97/storageservice/file/controller/FileController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -40,7 +41,7 @@ public class FileController {
     return ResponseEntity.status(HttpStatus.CREATED).body(ret);
   }
 
-  @PostMapping(path = "/{id}")
+  @PutMapping(path = "/{id}")
   public ResponseEntity<?> update(@RequestPart(name = "attachment") MultipartFile mpf,
       @PathVariable long id) {
 
